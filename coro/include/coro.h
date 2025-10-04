85ef6fbb36479dad127448e2262e78d739781571
Yao Noel ACHI <achi.noel@hotmail.com>
2025-10-05 01:53:33 +0200

    chore: add specialization for void coroutines

--- coro/include/coro.h
+++ coro/include/coro.h
@@ -8,7 +8,7 @@
 #include "spdlog/spdlog.h"
 
 namespace kio {
-    template<typename T = void>
+    template<typename T>
     struct Task {
         struct promise_type;
 
@@ -72,12 +72,6 @@ namespace kio {
             {
                 result_ = std::move(value);
             }
-
-            // void return_void()
-            //     requires std::is_void_v<T>
-            // {
-            //     result_ = std::monostate{};
-            // }
         };
 
         [[nodiscard]]
@@ -105,6 +99,78 @@ namespace kio {
             }
         }
     };
+
+        // Specialization for Task<void>
+    template<>
+    struct Task<void> {
+        struct promise_type;
+
+        std::coroutine_handle<promise_type> h_;
+
+        explicit Task(std::coroutine_handle<promise_type> h) : h_(h) {}
+        Task(Task &&other) noexcept : h_(std::exchange(other.h_, {})) {}
+        Task(const Task &) = delete;
+        Task &operator=(const Task &) = delete;
+        Task &operator=(Task &&other) noexcept {
+            if (this != &other) {
+                if (h_) h_.destroy();
+                h_ = other.h_;
+                other.h_ = nullptr;
+            }
+            return *this;
+        }
+        ~Task() {
+            if (h_) {
+                h_.destroy();
+            }
+        }
+
+        struct promise_type {
+            // Variant without T for the void case
+            std::variant<std::monostate, std::exception_ptr> result_;
+            std::coroutine_handle<> continuation_;
+
+            [[nodiscard]]
+            Task get_return_object() noexcept {
+                return Task{std::coroutine_handle<promise_type>::from_promise(*this)};
+            }
+
+            std::suspend_always initial_suspend() noexcept { return {}; }
+
+            struct final_awaiter {
+                bool await_ready() noexcept { return false; }
+                std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> h) noexcept {
+                    if (auto continuation = h.promise().continuation_)
+                        return continuation;
+                    return std::noop_coroutine();
+                }
+                void await_resume() noexcept {}
+            };
+
+            final_awaiter final_suspend() noexcept { return {}; }
+            void unhandled_exception() { result_ = std::current_exception(); }
+
+            void return_void() {}
+        };
+
+        [[nodiscard]]
+        bool await_ready() const noexcept {
+            return h_.done();
+        }
+
+        std::coroutine_handle<> await_suspend(std::coroutine_handle<> awaiting_coroutine) noexcept {
+            h_.promise().continuation_ = awaiting_coroutine;
+            return h_;
+        }
+
+        // await_resume returns void
+        void await_resume() {
+            if (h_.promise().result_.index() == 1) {
+                std::rethrow_exception(std::get<1>(h_.promise().result_));
+            }
+        }
+    };
+
 
     struct DetachedTask {
         struct promise_type {
